7cc759c0bafccc217b82a0c5a400d7e17a9c1d79
PolinaFam <45046398+PolinaFam@users.noreply.github.com>
2019-12-22 22:06:07 +0700

    first commit

---
+++ precache-manifest.d3181e85de7fbdc37261af062bf68f21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d2663dce23946811e97126d66cac43",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01bf9262f65567effb8d",
+    "url": "./static/css/2.6749d01e.chunk.css"
+  },
+  {
+    "revision": "d8e9a65453f899dcce5a",
+    "url": "./static/css/main.523f4a36.chunk.css"
+  },
+  {
+    "revision": "01bf9262f65567effb8d",
+    "url": "./static/js/2.5e2ca1d8.chunk.js"
+  },
+  {
+    "revision": "d8e9a65453f899dcce5a",
+    "url": "./static/js/main.62ea1c17.chunk.js"
+  },
+  {
+    "revision": "bf21f57192535a95f46d",
+    "url": "./static/js/runtime-main.14d6293a.js"
+  }
+]);
